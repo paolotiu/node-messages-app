317c2bd27c04ab7c41657de0318052fbdf795fcd
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-11-28 14:30:29 +0800

    added routes for sign-up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+const userController = require('../controllers/userController');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('home', { title: 'Express' });
 });
+
+// Get login form
+router.get('/login', userController.login_get);
+
+//Get sign-up form
+router.get('/sign-up', userController.sign_up_get);
+
+//POST sign-up form / Save user
+router.post('/sign-up', userController.sign_up_post);
 
 module.exports = router;
