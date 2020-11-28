ea68b7db7922516e28a6d952752c1e9e0a01e65a
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-11-28 15:24:42 +0800

    Fixed typo in model

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     firstname: String,
     lastname: String,
-    usernames: String,
+    username: String,
+    password: String,
     status: { type: String, enum: ['user', 'admin'] },
 });
 
